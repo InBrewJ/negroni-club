016ebce2dc0387aea5b43155503f82a4e4e4a7f4
InBrewJ <jason@urawizard.com>
2024-12-30 21:19:01 +0000

    Adds the CockroachDbTest function to rest-api-infra. Turns out the cdktf code generation thing with random providers / modules works quite smoothly once the import paths are worked out

--- nqdi/infra/rest-api-infra/main.go
+++ nqdi/infra/rest-api-infra/main.go
@@ -7,6 +7,8 @@ import (
 
 	"log"
 
+	crdb_cluster "cdk.tf/go/stack/generated/cockroachdb/cockroach/cluster"
+	crdb "cdk.tf/go/stack/generated/cockroachdb/cockroach/provider"
 	alb "github.com/cdktf/cdktf-provider-aws-go/aws/v19/alb"
 	alblistener "github.com/cdktf/cdktf-provider-aws-go/aws/v19/alblistener"
 	albtargetgroup "github.com/cdktf/cdktf-provider-aws-go/aws/v19/albtargetgroup"
@@ -51,6 +53,38 @@ func NewMyStack(scope constructs.Construct, id string) cdktf.TerraformStack {
 
 	cdktf.NewTerraformOutput(stack, jsii.String("public_ip"), &cdktf.TerraformOutputConfig{
 		Value: instance.PublicIp(),
+	})
+
+	return stack
+}
+
+func CockroachDbTest(scope constructs.Construct, id string) cdktf.TerraformStack {
+	// provider added and go code generated with:
+	// cdktf provider add cockroachdb/cockroach
+	// correct import path for generated code living locally:
+	// "cdk.tf/go/stack/generated/cockroachdb/cockroach/provider"
+
+	env, err := godotenv.Read()
+
+	if err != nil {
+		log.Fatal("REST-API-INFRA ERROR: cannot load .env", err)
+	}
+
+	stack := cdktf.NewTerraformStack(scope, &id)
+
+	crdb.NewCockroachProvider(stack, jsii.String("Cockroach-test"), &crdb.CockroachProviderConfig{
+		Apikey: jsii.String(env["CRDB_API_KEY"]),
+	})
+
+	crdb_cluster.NewCluster(stack, jsii.String("crdb_cluster"), &crdb_cluster.ClusterConfig{
+		Name:             jsii.String("nqdi-delete-me"),
+		CloudProvider:    jsii.String("GCP"),
+		Plan:             jsii.String("BASIC"),
+		DeleteProtection: jsii.Bool(false),
+		Regions:          &[]*crdb_cluster.ClusterRegions{{Name: jsii.String("us-east1")}},
+		Serverless: &crdb_cluster.ClusterServerless{
+			SpendLimit: jsii.Number(20.0),
+		},
 	})
 
 	return stack
@@ -324,7 +358,8 @@ func RestApiInfraFargate(scope constructs.Construct, id string) cdktf.TerraformS
 
 func main() {
 	app := cdktf.NewApp(nil)
-	stack := RestApiInfraFargate(app, "rest_api_fargate")
+	// stack := RestApiInfraFargate(app, "rest_api_fargate")
+	stack := CockroachDbTest(app, "cockroachdb_test")
 	cdktf.NewRemoteBackend(stack, &cdktf.RemoteBackendConfig{
 		Hostname:     jsii.String("app.terraform.io"),
 		Organization: jsii.String("nqdi"),
